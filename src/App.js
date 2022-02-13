57d02856af85c300d6761d8cfa7dcd1f563ecef5
studentbeginnerinjavaFx <seddik.kasraoui@esprit.tn>
2022-02-13 02:31:06 -1200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import img1 from './image/imageInSrc.jpg'
+class App extends Component {
+ constructor(){
+   super();
+   this.state = {
+    Person: {
+      fullName: "kassraoui Seddik",
+      bio: "I just love developpment",
+      imgSrc: img1,
+      profession: "web dev"
+    },
+    show: false
+  };
+ }
+  
 
-function App() {
+  ShowPerson = () => {
+    this.setState({
+      ...this.state,
+      show: !this.state.show
+    });
+  };
+ render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {this.state.show && (
+          <div>
+            <h1>{this.state.Person.fullName}</h1>
+            <h1>{this.state.Person.bio}</h1>
+            <img src={this.state.Person.imgSrc} alt="image"/>
+            <h1>{this.state.Person.profession}</h1>
+            <br/>
+          </div>
+        )}
+
+        <button onClick={this.ShowPerson}>Check state</button>
     </div>
   );
+ }
 }
 
 export default App;
